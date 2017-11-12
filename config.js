545ba24287b7d65864dc966b38bbf33a5f2e991b
VishalSwarnkar <vishalswa@gmail.com>
2017-11-12 17:38:24 +0000

    facebook passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey' : '12345-67890-45639-3453',
-  'mongoUrl' : 'mongodb://localhost:27017/confusion'
+  'mongoUrl' : 'mongodb://localhost:27017/confusion',
+  'facebook' : {
+    clientId: '138044603520592',
+    clientSecret: 'c24f8a69b769dfbcda7e3399ba045075'
+  }
 }
